178c0958169f40ed62bd1bb3721d36a7a0001a0d
Justin4587 <Thurman@atlasok.com>
2021-02-02 20:54:19 -0600

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,28 +7,28 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *master;
+	listint_t *master;
 
-    master = (*list)->next;
-    while (master)
-    {
-        while (master->prev && master->n < master->prev->n)
-        {
-            master->prev->next = master->next;
-            if (master->next)
-                master->next->prev = master->prev;
-            master->next = master->prev;
-            master->prev = master->prev->prev;
-            master->next->prev = master;
+	master = (*list)->next;
+	while (master)
+	{
+		while (master->prev && master->n < master->prev->n)
+		{
+			master->prev->next = master->next;
+			if (master->next)
+				master->next->prev = master->prev;
+			master->next = master->prev;
+			master->prev = master->prev->prev;
+			master->next->prev = master;
 
-            if (master->prev == NULL)
-                *list = master;
-            else
-            {
-                master->prev->next = master;
-            }
-            print_list(*list);
-        }
-        master = master->next;
-    }
+			if (master->prev == NULL)
+				*list = master;
+			else
+			{
+				master->prev->next = master;
+			}
+			print_list(*list);
+		}
+		master = master->next;
+	}
 }
