df124e3f06bffae1a85566753a29a28f86bb28b6
Justin4587 <Thurman@atlasok.com>
2021-02-02 19:13:19 -0600

    sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - insert
+ * @list: adoklp
+ * Return: Void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - more sorting
+ * @array: an array
+ * @size: size of
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+    unsigned int runner, swap, i;
+    int temp;
+
+    if(!array)
+        return;
+
+    for (runner = 0; runner < size - 1; runner++)
+    {
+        i = runner;
+        for (swap = runner + 1; swap < size; swap++)
+            if (array[swap] < array[i])
+                i = swap;
+            if (i != runner)
+            {
+                temp = array[i];
+                array[i] = array[runner];
+                array[runner] = temp;
+                print_array(array, size);
+            }
+    }
+}
